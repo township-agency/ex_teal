4b96d70b68871f4d6141b456b2e906605b459a1f
Scott Taylor <staylorwr@users.noreply.github.com>
2020-11-02 09:10:37 -0500

    feat: Enable Naive DateTime Fields
    Allow a DateTime to represent a naive_datetime that does not have a timezone and does not use browser timezones to configure the values (#36)

--- assets/src/components/Form/DateTime.vue
+++ assets/src/components/Form/DateTime.vue
@@ -11,6 +11,7 @@
         class="w-full form-control form-input form-input-bordered"
         :date-format="pickerFormat"
         :placeholder="placeholder"
+        :twelve-hour-time="twelveHourTime"
         @change="handleChange"
       />
     </template>
@@ -39,6 +40,10 @@ export default {
 
     pickerFormat () {
       return this.field.options.picker_format || 'Y-m-d H:i:S';
+    },
+
+    twelveHourTime () {
+      return !this.field.options.twenty_four_hour_time || true;
     }
   },
 
@@ -48,16 +53,32 @@ export default {
 
       if (this.value !== '') {
         this.localizedValue = this.fromUTC(this.value);
+        return;
+      }
+
+      if (this.naiveDateTime) {
+        this.localizedValue = this.value;
       }
     },
 
     handleChange (value) {
+      if (this.naiveDateTime) {
+        const now = DateTime.local();
+        const dt = DateTime.fromISO(value).setZone(now.zoneName, {
+          keepLocalTime: true
+        });
+
+        this.value = `${dt.toFormat('yyyy-M-dd')}T${dt.toFormat('TT')}`;
+        this.localizedValue = dt.toLocaleString(DateTime.DATETIME_MED);
+
+        return;
+      }
       this.value = value;
       this.localizedValue = this.fromUTC(this.value);
     },
 
     fromUTC (value) {
-      return DateTime.fromISO(this.value).toLocaleString(DateTime.DATETIME_FULL);
+      return DateTime.fromISO(value).toLocaleString(DateTime.DATETIME_FULL);
     }
   }
 };
