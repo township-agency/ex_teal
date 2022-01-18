3d3b8ca05e61802b62d457d7965c2e78ec154b13
Scott Taylor <staylorwr@users.noreply.github.com>
2022-01-18 13:41:31 -0500

    feat: Add Rich MultiSelect functionality

--- lib/ex_teal/fields/multi_select.ex
+++ lib/ex_teal/fields/multi_select.ex
@@ -7,19 +7,43 @@ defmodule ExTeal.Fields.MultiSelect do
       MultiSelect.make(:regions)
       |> MultiSelect.with_options([%{value: 1, label: "USA"}, %{value: 2, label: "EU"}])
 
+  You can also return a list of maps with `value`, `title`, `subtitle` and `thumbnail` keys
+  to use 'card' style taggables, useful when the multiselect represents a relationship.
   """
 
+  @type simple_option :: %{
+          value: :integer | String.t(),
+          label: String.t()
+        }
+
+  @type card_option :: %{
+          required(:value) => :integer | String.t(),
+          required(:title) => String.t(),
+          optional(:subtitle) => String.t(),
+          optional(:thumbnail) => String.t()
+        }
+
+  @type valid_option :: simple_option | card_option
+
+  @type valid_options :: list(valid_option)
+
   use ExTeal.Field
+  alias ExTeal.Field
 
   def component, do: "multi-select"
 
   def default_sortable, do: false
 
+  @spec with_options(Field.t(), valid_options | (() -> valid_options)) :: Field.t()
   def with_options(field, options) when is_list(options) do
-    %{field | options: options, private_options: %{display_using_labels: false}}
+    %{field | options: options}
   end
 
   def with_options(field, options_fn) when is_function(options_fn) do
+    IO.warn(
+      "with_options/2 callback is depreciated, and will be removed in 1.0.  See `ExTeal.Fields.MultiSelect.with_options/2`"
+    )
+
     options = options_fn.()
     with_options(field, options)
   end
