ee3cb427e0f458388ee99eb026d9c83a62906f70
Scott Taylor <staylorwr@users.noreply.github.com>
2022-01-20 13:54:50 -0500

    fix: show empty status

--- assets/src/components/Detail/MultiSelect.vue
+++ assets/src/components/Detail/MultiSelect.vue
@@ -4,41 +4,44 @@
       slot="value"
       class="text-gray-darkest"
     >
-      <span
-        v-for="v in field.value"
-        :key="v.value"
-        class="inline-block mr-2 border border-rounded-sm px-2 py-1"
-      >
-        <div
-          v-if="v.title"
-          slot="default"
-          class="flex items-center"
+      <span v-if="field.value.length">
+        <span
+          v-for="v in field.value"
+          :key="v.value"
+          class="inline-flex items-center mr-2 border border-rounded-sm px-2 py-1 mb-2"
         >
           <div
-            v-if="v.thumbnail"
-            class="mr-3"
+            v-if="v.title"
+            slot="default"
+            class="flex items-center"
           >
-            <img
-              :src="v.thumbnail"
-              class="w-6 h-6 rounded block"
+            <div
+              v-if="v.thumbnail"
+              class="mr-3"
             >
+              <img
+                :src="v.thumbnail"
+                class="w-6 h-6 rounded block"
+              >
+            </div>
+            <div class="flex">
+              <p class="text-gray-darkest mr-2">
+                {{ v.title }}
+              </p>
+              <p
+                v-if="v.subtitle"
+                class="text-xs mt-1 opacity-75"
+              >
+                {{ v.subtitle }}
+              </p>
+            </div>
           </div>
-          <div class="flex">
-            <p class="text-gray-darkest mr-2">
-              {{ v.title }}
-            </p>
-            <p
-              v-if="v.subtitle"
-              class="text-xs mt-1 opacity-75"
-            >
-              {{ v.subtitle }}
-            </p>
-          </div>
-        </div>
-        <span v-else>
-          {{ v.label }}
+          <span v-else>
+            {{ v.label }}
+          </span>
         </span>
       </span>
+      <span v-else>&mdash;</span>
     </p>
   </panel-item>
 </template>
