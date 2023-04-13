9a0d32399857040ced9c3f39c2e1a2b6d3c2849c
Scott Taylor <staylorwr@users.noreply.github.com>
2023-04-12 21:28:25 -0400

    [s]

--- lib/ex_teal/resource/pagination.ex
+++ lib/ex_teal/resource/pagination.ex
@@ -3,6 +3,7 @@ defmodule ExTeal.Resource.Pagination do
   Used to build paginated views of resources
   """
   import Ecto.Query
+
   def paginate_query(query, conn, resource) do
     per_page = conn.params |> Map.get("per_page", "25") |> String.to_integer()
     page = conn.params |> Map.get("page", "1") |> String.to_integer()
@@ -10,10 +11,12 @@ defmodule ExTeal.Resource.Pagination do
     offset = (page - 1) * per_page
 
     data_query = from(query, limit: ^per_page, offset: ^offset)
+
     agg_query =
       query
       |> exclude(:preload)
       |> exclude(:select)
+
     agg_query = from(q in subquery(agg_query), select: count(q.id))
     all = resource.repo().one(agg_query)
 
