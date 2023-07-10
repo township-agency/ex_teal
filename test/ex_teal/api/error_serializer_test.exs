7993d603a548c0485a14ead99ee0ab4d67cf5537
Scott Taylor <staylorwr@users.noreply.github.com>
2023-07-10 15:37:27 -0400

    fix: Serializer should consider errors where the key is an array

--- test/ex_teal/api/error_serializer_test.exs
+++ test/ex_teal/api/error_serializer_test.exs
@@ -1,10 +1,24 @@
 defmodule ExTeal.Api.ErrorSerializerTest do
   use ExUnit.Case, async: true
+  alias Ecto.Changeset
 
   alias ExTeal.Api.ErrorSerializer
 
   test "flatten/1 returns a flattened map" do
     assert ErrorSerializer.flatten(%{foo: %{bar: "baz"}}) == %{"foo.bar" => "baz"}
     assert ErrorSerializer.flatten(%{foo: %{bar: ["baz"]}}) == %{"foo.bar" => ["baz"]}
+  end
+
+  test "translate_errors/1" do
+    data = %{}
+    types = %{name: :string}
+    params = %{name: nil}
+
+    changeset =
+      {data, types}
+      |> Changeset.cast(params, Map.keys(types))
+      |> Changeset.validate_required(Map.keys(types))
+
+    assert ErrorSerializer.translate_errors(changeset) == %{name: ["can't be blank"]}
   end
 end
